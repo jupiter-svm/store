183349815732e70eacefe2b801d0b1cfd3e27f7c
jupiter-svm <andr@inbox.ru>
2014-07-24 16:08:25 +0400

    Товары
    Добавлена возможность редактирования товаров

--- models/ProductsModel.php
+++ models/ProductsModel.php
@@ -110,3 +110,67 @@ function insertProduct($itemName, $itemPrice, $itemDesc, $itemCat) {
     
     return $rs;
 }
+
+/**
+ * Функция обновления данных о продукте
+ * 
+ * @global type $db - подключение к БД
+ * @param type $itemId - идентификатор товара
+ * @param type $itemName - название товара
+ * @param type $itemPrice - цена товара
+ * @param type $itemStatus - статус товара
+ * @param type $itemDesc - описание товара
+ * @param type $itemCat - категория товара
+ * @param type $newFileName - новое имя файла
+ * @return type
+ */
+function updateProduct($itemId, $itemName, $itemPrice, 
+                            $itemStatus, $itemDesc, $itemCat, $newFileName=null) {
+    global $db;
+    
+    $set=array();
+    
+    if($itemName) {
+        $set[]="`name`='{$itemName}'";
+    }
+    
+    if($itemPrice>0) {
+        $set[]="`price`='{$itemPrice}'";
+    }
+    
+    if($itemStatus!==null) {
+        $set[]="`status`='{$itemStatus}'";
+    }
+    
+    if($itemDesc) {
+        $set[]="`description`='{$itemDesc}'";
+    }
+    
+    if($itemCat) {
+        $set[]="`category_id`='{$itemCat}'";
+    }
+    
+    if($newFileName) {
+        $set[]="`image`='{$newFileName}'";
+    }
+     
+    $setStr=implode($set, ", ");
+    
+    $sql="UPDATE products SET {$setStr} WHERE id='{$itemId}'";
+    
+    $rs=mysqli_query($db, $sql);
+    
+    return $rs;
+}
+
+/**
+ * Функция обновления данных о картинке
+ * 
+ * @param type $itemId - идентификатор товара
+ * @param type $newFileName - название сохраняемого файла
+ */
+function updateProductImage($itemId, $newFileName) {
+    $rs=updateProduct($itemId, null, null, null, null, null, $newFileName);
+    
+    return $rs;
+}
